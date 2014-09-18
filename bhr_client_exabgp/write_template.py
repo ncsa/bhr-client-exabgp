017c52c7695df4f12d7b59f2d4db618cdb13a0b0
Justin Azoff <jazoff@illinois.edu>
2014-09-18 09:17:54 -0400

    package

---
+++ bhr_client_exabgp/write_template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import socket
+from mako.template import Template
+
+from bhr_client.rest import Client
+
+def render_config():
+    c = Client(ident=None)
+    t = Template(filename=os.getenv("BHR_TEMPLATE"))
+
+    context = {
+        "blocked": list(c.get_list()),
+        'ip': socket.gethostbyaddr(socket.gethostname())[2][0]
+    }
+
+    return t.render(**context)
+
+def main():
+    print render_config()
+
+if __name__ == "__main__":
+    print render_config()
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='bhr-client-exabgp',
+    version=version,
+    description="BHR Client",
+    long_description="",
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='passive dns',
+    author='Justin Azoff',
+    author_email='JAzoff@illinois.edu',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "requests>=2.0",
+        "bhr_client>=0.1"
+    ],
+    entry_points = {
+        'console_scripts': [
+            'bhr-client-exabgp-process = bhr_client_exabgp.process:main',
+            'bhr-client-exabgp-write-template = bhr_client_exabgp.write_template:main',
+        ]
+    }
+)
