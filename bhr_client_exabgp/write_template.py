a4184049dc078442620d3865161ddeec45b4f85d
Justin Azoff <jazoff@illinois.edu>
2018-06-27 15:31:12 -0500

    problem: dual stack hosts are not properly supported.
    use gethostbyaddr to discover the proper v4 and v6 addresses for the
    host and make both available in the template

--- bhr_client_exabgp/write_template.py
+++ bhr_client_exabgp/write_template.py
@@ -7,6 +7,18 @@ from mako.template import Template
 
 from bhr_client.rest import login_from_env
 
+def get_ips():
+    v4 = None
+    v6 = None
+    for res in socket.getaddrinfo(socket.gethostname(), 80):
+        af, socktype, proto, canonname, sa = res
+        if af == socket.AF_INET6:
+            v6 = sa[0]
+        else:
+            v4 = sa[0]
+
+    return v4, v6
+
 def render_config():
     c = login_from_env()
     t = Template(filename=os.getenv("BHR_TEMPLATE"))
@@ -15,9 +27,11 @@ def render_config():
     if not path_to_bhr_client_exabgp_loop:
         raise RuntimeError("Can not find bhr-client-exabgp-loop in $PATH")
 
+    ipv4, ipv6 = get_ips()
     context = {
         "blocked": [],
-        'ip': socket.gethostbyaddr(socket.gethostname())[2][0],
+        'ip': ipv4,
+        'ipv6': ipv6,
         "path_to_bhr_client_exabgp_loop": path_to_bhr_client_exabgp_loop,
     }
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from glob import glob
 
-version = '0.6'
+version = '0.7'
 
 setup(name='bhr-client-exabgp',
     version=version,
